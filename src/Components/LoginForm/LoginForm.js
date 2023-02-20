243a2ebf2c6ab2d069e558ef4e649243861b69fd
Bhavesh588 <sbhavesh588@gmail.com>
2023-02-20 11:19:07 +0530

    Adding Validation to Signup and Login form

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,113 +1,144 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Field, Form, Formik } from "formik";
 import React from "react";
 
 import "./LoginForm.scss";
 
 function LoginForm({ login_txt, login_box, setAccount }) {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("Handle Submit");
+    const validate = (values) => {
+        const errors = {};
+
+        var mailFormat = /\S+@\S+\.\S+/;
+        if (!values.email) {
+            errors.email = "Required";
+        } else if (!values.email.match(mailFormat)) {
+            errors.email = "Invalid Email address!";
+        }
+        if (!values.password) errors.password = "Required";
+
+        return errors;
+    };
+
+    const initialValues = {
+        email: "",
+        password: "",
+    };
+
+    const onSubmit = (values, { resetForm }) => {
+        alert(JSON.stringify(values, null, 2));
+        resetForm();
     };
 
     return (
-        <div className="loginform w-100">
-            <form
-                onSubmit={handleSubmit}
-                className="w-100 d-flex flex-column justify-content-center align-items-center"
+        <div className="loginform w-100 d-flex flex-column justify-content-center align-items-center">
+            <Formik
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                validate={validate}
+                enableReinitialize={true}
             >
-                <h2>Login</h2>
-                <div className="inputbox d-flex flex-column">
-                    <input
-                        type="text"
-                        placeholder="Email"
-                        className="input_text"
-                        style={{
-                            color: `${login_txt}`,
-                            borderColor: `${login_txt}`,
-                        }}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        className="input_text"
-                        style={{
-                            color: `${login_txt}`,
-                            borderColor: `${login_txt}`,
-                        }}
-                    />
-                </div>
-                <div className="not_account">
-                    <button
-                        style={{
-                            color: `${login_txt}`,
-                        }}
-                    >
-                        Forgot Password?
-                    </button>
-                </div>
+                {(props) => (
+                    <Form className="w-100 d-flex flex-column justify-content-center align-items-center">
+                        <h2>Login</h2>
+                        <div className="inputbox d-flex flex-column">
+                            <Field
+                                type="text"
+                                name="email"
+                                placeholder="Email"
+                                className="input_text"
+                                style={{
+                                    color: `${login_txt}`,
+                                    borderColor: `${login_txt}`,
+                                }}
+                            />
+                            {props.errors.email && props.touched.email ? (
+                                <span className="text-danger">
+                                    {props.errors.email}
+                                </span>
+                            ) : null}
+                            <Field
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                className="input_text"
+                                style={{
+                                    color: `${login_txt}`,
+                                    borderColor: `${login_txt}`,
+                                }}
+                            />
+                            {props.errors.password && props.touched.password ? (
+                                <span className="text-danger">Required</span>
+                            ) : null}
+                        </div>
+                        <div className="not_account">
+                            <button
+                                style={{
+                                    color: `${login_txt}`,
+                                }}
+                            >
+                                Forgot Password?
+                            </button>
+                        </div>
+                        <button
+                            type="submit"
+                            className="login_btn"
+                            style={{
+                                color: `${login_txt}`,
+                                borderColor: `${login_txt}`,
+                            }}
+                        >
+                            Login
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+            <div className="not_account">
+                <span>Don't have an account?</span>
                 <button
-                    className="login_btn"
+                    onClick={() => setAccount(true)}
                     style={{
                         color: `${login_txt}`,
-                        borderColor: `${login_txt}`,
                     }}
                 >
-                    Login
+                    Create Account
                 </button>
-                <div className="not_account">
-                    <span>Don't have an account?</span>
-                    <button
-                        onClick={() => setAccount(true)}
-                        style={{
-                            color: `${login_txt}`,
-                        }}
-                    >
-                        Create Account
-                    </button>
-                </div>
-                <div className="w-100 d-flex flex-column justify-content-center align-items-center">
-                    <hr
-                        style={{
-                            backgroundColor: `${login_txt}`,
-                        }}
-                    />
-                    <span
-                        className="or_text"
-                        style={{
-                            backgroundColor: `${login_box}`,
-                        }}
-                    >
-                        OR
-                    </span>
-                </div>
-                <div className="btn-all facebook-btn d-flex align-items-center">
-                    <FontAwesomeIcon
-                        icon="fa-brands fa-facebook-f"
-                        className="mx-2"
-                    />
-                    <span className="w-100 d-flex justify-content-center">
-                        Login with Facebook
-                    </span>
-                </div>
-                <div className="btn-all google-btn d-flex align-items-center">
-                    <FontAwesomeIcon
-                        icon="fa-brands fa-google"
-                        className="mx-2"
-                    />
-                    <span className="w-100 d-flex justify-content-center">
-                        Login with Google
-                    </span>
-                </div>
-                <div className="btn-all twitter-btn d-flex align-items-center">
-                    <FontAwesomeIcon
-                        icon="fa-brands fa-twitter"
-                        className="mx-2"
-                    />
-                    <span className="w-100 d-flex justify-content-center">
-                        Login with Twitter
-                    </span>
-                </div>
-            </form>
+            </div>
+            <div className="w-100 d-flex flex-column justify-content-center align-items-center">
+                <hr
+                    style={{
+                        backgroundColor: `${login_txt}`,
+                    }}
+                />
+                <span
+                    className="or_text"
+                    style={{
+                        backgroundColor: `${login_box}`,
+                    }}
+                >
+                    OR
+                </span>
+            </div>
+            <div className="btn-all facebook-btn d-flex align-items-center">
+                <FontAwesomeIcon
+                    icon="fa-brands fa-facebook-f"
+                    className="mx-2"
+                />
+                <span className="w-100 d-flex justify-content-center">
+                    Login with Facebook
+                </span>
+            </div>
+            <div className="btn-all google-btn d-flex align-items-center">
+                <FontAwesomeIcon icon="fa-brands fa-google" className="mx-2" />
+                <span className="w-100 d-flex justify-content-center">
+                    Login with Google
+                </span>
+            </div>
+            <div className="btn-all twitter-btn d-flex align-items-center">
+                <FontAwesomeIcon icon="fa-brands fa-twitter" className="mx-2" />
+                <span className="w-100 d-flex justify-content-center">
+                    Login with Twitter
+                </span>
+            </div>
         </div>
     );
 }
--- src/Components/SignupForm/SignupForm.js
+++ src/Components/SignupForm/SignupForm.js
@@ -1,70 +1,118 @@
+import { Field, Form, Formik } from "formik";
 import React from "react";
 
 import "./SignupForm.scss";
 
 function SignupForm({ login_txt, setAccount }) {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("Handle Submit");
+    const validate = (values) => {
+        const errors = {};
+
+        var mailFormat = /\S+@\S+\.\S+/;
+        if (!values.email.match(mailFormat))
+            errors.email = "Invalid Email address!";
+        if (!values.email) errors.email = "Required";
+        if (!values.password) errors.password = "Required";
+        if (values.password !== values.confirmPassword)
+            errors.confirmPassword = "Password does not match";
+        if (!values.confirmPassword) errors.confirmPassword = "Required";
+
+        return errors;
+    };
+
+    const initialValues = {
+        email: "",
+        password: "",
+        confirmPassword: "",
+    };
+
+    const onSubmit = (values, { resetForm }) => {
+        alert(JSON.stringify(values, null, 2));
+        resetForm();
     };
 
     return (
-        <div className="signup w-100">
-            <form
-                onSubmit={handleSubmit}
-                className="w-100 d-flex flex-column justify-content-center align-items-center"
+        <div className="signup w-100 d-flex flex-column justify-content-center align-items-center">
+            <Formik
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                validate={validate}
+                enableReinitialize={true}
             >
-                <h2>Sign Up</h2>
-                <div className="inputbox d-flex flex-column">
-                    <input
-                        type="text"
-                        placeholder="Email"
-                        className="input_text"
-                        style={{
-                            color: `${login_txt}`,
-                            borderColor: `${login_txt}`,
-                        }}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        className="input_text"
-                        style={{
-                            color: `${login_txt}`,
-                            borderColor: `${login_txt}`,
-                        }}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Confirm Password"
-                        className="input_text"
-                        style={{
-                            color: `${login_txt}`,
-                            borderColor: `${login_txt}`,
-                        }}
-                    />
-                </div>
+                {(props) => (
+                    <Form className="w-100 d-flex flex-column justify-content-center align-items-center">
+                        <h2>Sign Up</h2>
+                        <div className="inputbox d-flex flex-column">
+                            <Field
+                                type="text"
+                                placeholder="Email"
+                                name="email"
+                                className="input_text"
+                                style={{
+                                    color: `${login_txt}`,
+                                    borderColor: `${login_txt}`,
+                                }}
+                            />
+                            {props.errors.email && props.touched.email ? (
+                                <span className="text-danger">
+                                    {props.errors.email}
+                                </span>
+                            ) : null}
+                            <Field
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                className="input_text"
+                                style={{
+                                    color: `${login_txt}`,
+                                    borderColor: `${login_txt}`,
+                                }}
+                            />
+                            {props.errors.password && props.touched.password ? (
+                                <span className="text-danger">
+                                    {props.errors.password}
+                                </span>
+                            ) : null}
+                            <Field
+                                type="password"
+                                placeholder="Confirm Password"
+                                name="confirmPassword"
+                                className="input_text"
+                                style={{
+                                    color: `${login_txt}`,
+                                    borderColor: `${login_txt}`,
+                                }}
+                            />
+                            {props.errors.confirmPassword &&
+                            props.touched.confirmPassword ? (
+                                <span className="text-danger">
+                                    {props.errors.confirmPassword}
+                                </span>
+                            ) : null}
+                        </div>
+                        <button
+                            type="submit"
+                            className="login_btn"
+                            style={{
+                                color: `${login_txt}`,
+                                borderColor: `${login_txt}`,
+                            }}
+                        >
+                            Signup
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+            <div className="not_account">
+                <span>Already have an account?</span>
                 <button
-                    className="login_btn"
+                    onClick={() => setAccount(false)}
                     style={{
                         color: `${login_txt}`,
-                        borderColor: `${login_txt}`,
                     }}
                 >
-                    Signup
+                    Login
                 </button>
-                <div className="not_account">
-                    <span>Already have an account?</span>
-                    <button
-                        onClick={() => setAccount(false)}
-                        style={{
-                            color: `${login_txt}`,
-                        }}
-                    >
-                        Login
-                    </button>
-                </div>
-            </form>
+            </div>
         </div>
     );
 }
